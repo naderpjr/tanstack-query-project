23bf281a5c8998c59dfd8904d5e5d19091b02f92
Nader PJR <pjrnader@gmail.com>
2026-01-02 21:13:04 +0430

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useQuery } from '@tanstack/react-query';
+
+const fetchPosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!response.ok) throw new Error("Error fetching data");
+  return response.json();
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { data, isLoading, error } = useQuery({ queryKey: ['post'], queryFn: fetchPosts })
+
+  if (isLoading) return <p> Loading...</p>
+  if (error) return <p> Error occured: {error.message}</p>
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {data.map((post) => (
+        <p>{post.title}</p>
+      ))}
     </>
   )
 }
